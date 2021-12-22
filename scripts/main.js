cd593a5c73fcad7e5eedb61e4b96257b31e0484a
boudewijnbout <boudewijn_bout@outlook.com>
2021-12-22 14:12:15 +0100

    Renamed classname and better animation propety

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // Hamburger toggle
 const hamburger = document.querySelector('.hamburger');
-const mobileMenu = document.querySelector('.nav-menu-desktop');
+const mobileMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', function() {
     mobileMenu.classList.toggle('active');
